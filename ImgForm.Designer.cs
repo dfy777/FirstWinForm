9906295868f46d60247b28e785d3705f0e05d292
Mirai <wr568367887@gmail.com>
2020-10-03 23:46:42 +0800

    demo the use of listview and picture box. display picture which in desktop on the windows and use listview to select distinct picture to display on the window

--- ImgForm.Designer.cs
+++ ImgForm.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView = new System.Windows.Forms.ListView();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(77, 74);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(121, 97);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView_MouseDown);
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(469, 117);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(134, 126);
+            this.picBox.TabIndex = 1;
+            this.picBox.TabStop = false;
             // 
             // ImgForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.listView);
             this.Name = "ImgForm";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.ImgForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.PictureBox picBox;
     }
 }
--- ImgForm.cs
+++ ImgForm.cs
@@ -20,8 +20,15 @@ namespace FirstWinForm
 
         private void ImgForm_Load(object sender, EventArgs e)
         {
-            var images = GetImageFileNameList();
-            ShowImages(images);
+            ///寻找桌面上的png图片并排列显示在窗口中
+            //var images = GetImageFileNameList();
+            //ShowImages(images);
+            
+
+            ///用listview列举出桌面png图片
+            ///选择相应的图片并显示
+            PopulateListView();
+            InitializePictureBox();
         }
 
 
@@ -36,16 +43,18 @@ namespace FirstWinForm
                 if (index % 4 == 0 && index > 0)
                 {
                     x = 0;
-                    y += 110;
+                    y += 150;
                 }
 
                 PictureBox picbox = new PictureBox();
-                picbox.Size = new System.Drawing.Size(100, 100);
+                picbox.Size = new System.Drawing.Size(140, 140);
                 picbox.Location = new Point(x, y);
+                picbox.SizeMode = PictureBoxSizeMode.StretchImage;      //拉伸
+                picbox.BorderStyle = BorderStyle.Fixed3D;
 
                 picbox.Load(item);
                 this.Controls.Add(picbox);
-                x += 110;
+                x += 145;
                 index += 1;
             }
         }
@@ -56,5 +65,87 @@ namespace FirstWinForm
             var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             return Directory.GetFiles(path, "*.png", SearchOption.TopDirectoryOnly).ToList(); ;
         }
+
+
+        private void PopulateListView()
+        {
+            listView.Width = 270;
+            listView.Height = 300;
+            listView.Location = new System.Drawing.Point(10, 10);
+
+            // Declare and construct the ColumnHeader objects.
+            ColumnHeader header1, header2;
+            header1 = new ColumnHeader();
+            header2 = new ColumnHeader();
+
+            // Set the text, alignment and width for each column header.
+            header1.Text = "File name";
+            header1.TextAlign = HorizontalAlignment.Left;
+            header1.Width = 70;
+
+            header2.TextAlign = HorizontalAlignment.Left;
+            header2.Text = "Location";
+            header2.Width = 200;
+
+            // Add the headers to the ListView control.
+            listView.Columns.Add(header1);
+            listView.Columns.Add(header2);
+
+            // Specify that each item appears on a separate line.
+            listView.View = View.Details;
+
+            // Populate the ListView.Items property.
+            // Set the directory to the sample picture directory.
+            DirectoryInfo dirInfo =
+                new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+            // Get the .jpg files from the directory
+            FileInfo[] files = dirInfo.GetFiles("*.png");
+
+            // Add each file name and full name including path
+            // to the ListView.
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.FullName);
+                    listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void InitializePictureBox()
+        {
+            //picBox = new PictureBox();
+
+            // Set the location and size of the PictureBox control.
+            this.picBox.Location = new System.Drawing.Point(350, 100);
+            this.picBox.Size = new System.Drawing.Size(140, 140);
+            this.picBox.TabStop = false;
+    
+            // Set the SizeMode property to the StretchImage value.  This
+            // will shrink or enlarge the image as needed to fit into
+            // the PictureBox.
+            this.picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Set the border style to a three-dimensional border.
+            this.picBox.BorderStyle = BorderStyle.Fixed3D;
+
+            // Add the PictureBox to the form.
+            //this.Controls.Add(this.picBox);
+        }
+
+        private void listView_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListViewItem selection = listView.GetItemAt(e.X, e.Y);
+
+            if (selection != null)
+            {
+                picBox.Image = Image.FromFile(selection.SubItems[1].Text);
+            }
+        }
+
+
     }
 }
